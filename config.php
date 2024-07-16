13396b3bb3991ee0f8d614150c4a0ae1a70e1727
BusBus997 <bigbuzart@gmail.com>
2024-07-16 23:00:37 +0400

    Added new CONST

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ define('DB_USER', 'YOUR_DB_USER');
 define('DB_PASSWORD', 'YOUR_DB_PASSWORD');
 define('DB_NAME', 'YOUR_DB_NAME');
 define('TOKEN', 'YOUR_TOKEN');
+define('YANDEX_TOKEN', 'YOUR_YANDEX_TOKEN');
